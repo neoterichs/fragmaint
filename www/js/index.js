a019d34ee61515a14ff4490d78f38e9b764de9b3
Ashwin Singh Tomar <makert40@gmail.com>
2015-07-31 16:49:19 +0530

    201507311649

--- www/js/index.js
+++ www/js/index.js
@@ -1,40 +1,22 @@
-// (c) 2014 Don Coleman
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/* global mainPage, deviceList, refreshButton */
-/* global detailPage, lightSwitch, dimmer, disconnectButton */
-/* global cordova, ble  */
-/* jshint browser: true , devel: true*/
 'use strict';
 
-var arrayBufferToInt = function (ab) {
-    var a = new Uint8Array(ab);
-    return a[0];
-};
+//global uuid id
+var global_deviceid = "";
 
-var robosmart = {
-    service: 'FFE0',
-    lightSwitch: 'FFE1',
-    brightness: 'FFE1',
-    powerConsumed: 'FFE1',
-    name: 'FFE1',
-    description: 'FFE1',
-    room: 'FFE1',
-    locationName: 'FFE1',
-    locationGps: 'FFE1',
-    disconnect: 'FFE1',
-};
+//string to ascii
+function stringToBytes(string) {
+   var array = new Uint8Array(string.length);
+   for (var i = 0, l = string.length; i < l; i++) {
+	   array[i] = string.charCodeAt(i);
+	}
+	return array.buffer;
+}
+//ascii to string
+function bytesToString(buffer) {
+		return String.fromCharCode.apply(null, new Uint8Array(buffer));
+}
+
+var battery = {service: "FFE0",level: "2A19",char:"FFE1"};
 
 var app = {
     initialize: function() {
@@ -44,105 +26,56 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         refreshButton.addEventListener('touchstart', this.refreshDeviceList, false);
-        lightSwitch.addEventListener('change', this.setOnOff, false);
-        dimmer.addEventListener('change', this.setBrightness, false);
+        batteryStateButton.addEventListener('touchstart', this.readBatteryState, false);
         disconnectButton.addEventListener('touchstart', this.disconnect, false);
         deviceList.addEventListener('touchstart', this.connect, false); // assume not scrolling
-    },
+		sendButton.addEventListener('touchstart', this.sendData, false);
+		readButton.addEventListener('touchstart', this.readBatteryState, false);
+	},
     onDeviceReady: function() {
         app.refreshDeviceList();
     },
     refreshDeviceList: function() {
         deviceList.innerHTML = ''; // empties the list
-
-        if (cordova.platformId === 'android') {
-            // Bug: Android can't find RoboSmart when scanning for service
-            ble.scan([], 5, app.onDiscoverDevice, app.onError);
-        } else {
-            ble.scan([robosmart.service], 5, app.onDiscoverDevice, app.onError);
-        }
+        // scan for all devices
+        ble.scan([], 5, app.onDiscoverDevice, app.onError);
     },
     onDiscoverDevice: function(device) {
+		console.log(JSON.stringify(device));
         var listItem = document.createElement('li'),
             html = '<b>' + device.name + '</b><br/>' +
                 'RSSI: ' + device.rssi + '&nbsp;|&nbsp;' +
                 device.id;
 
-        listItem.dataset.deviceId = device.id;
+        listItem.dataset.deviceId = device.id;  // TODO
         listItem.innerHTML = html;
         deviceList.appendChild(listItem);
+		global_deviceid = device.id;
     },
     connect: function(e) {
         var deviceId = e.target.dataset.deviceId,
             onConnect = function() {
-                // subscribe for power consumption notifications
+				ble.notify(deviceId,battery.service,battery.char,app.onBatteryLevelChange,app.onError);
+				batteryStateButton.dataset.deviceId = deviceId;
                 disconnectButton.dataset.deviceId = deviceId;
-                lightSwitch.dataset.deviceId = deviceId;
-                dimmer.dataset.deviceId = deviceId;
                 app.showDetailPage();
-                app.syncUI(deviceId);
-            };
-
-        ble.connect(deviceId, onConnect, app.onError);
-    },
-    setOnOff: function(event) { // send data to robosmart
-
-        var success = function() {
-            console.log("success");
-        };
-
-        var failure = function() {
-            alert("Failed writing data to the robosmart");
-        };
-
-        var data = new Uint8Array(1);
-        data[0] = event.target.checked ? 0x1 : 0x0;
-        var deviceId = event.target.dataset.deviceId;
-
-        ble.write(deviceId, robosmart.service, robosmart.lightSwitch, data.buffer, success, failure);
-    },
-    setBrightness: function(event) { // send data to robosmart
-
-        var success = function() {
-            // if the light was off and brightness is adjusted, the light will turn on
-            lightSwitch.checked = true;
-            console.log("success");
-        };
-
-        var failure = function() {
-            alert("Failed writing data to the robosmart");
-        };
-
-        var deviceId = event.target.dataset.deviceId;
-
-        // pass the brightness from the slider to the light
-        var brightness = new Uint8Array(1);
-        brightness[0] = event.target.value;
-
-        ble.write(deviceId, robosmart.service, robosmart.brightness, brightness.buffer, success, failure);
-    },
-    syncUI: function(deviceId) {
-
-        var switchCallback = function(data) {
-            lightSwitch.checked = arrayBufferToInt(data) === 0x1;
-        };
-
-        var dimmerCallback = function(data) {
-            dimmer.value = arrayBufferToInt(data);
-        };
-
-        var failure = function(reason) {
-            console.log("Error syncing UI with the current state " + reason);
-        };
-
-        ble.read(deviceId, robosmart.service, robosmart.lightSwitch, switchCallback, failure);
-        ble.read(deviceId, robosmart.service, robosmart.brightness, dimmerCallback, failure);
-
+			};
+		ble.connect(deviceId,onConnect,app.onError);
+	},
+    onBatteryLevelChange: function(data) {
+        console.log(data);
+        var message;
+        var a = bytesToString(data);
+        batteryState.innerHTML = a;
     },
     disconnect: function(event) {
         var deviceId = event.target.dataset.deviceId;
         ble.disconnect(deviceId, app.showMainPage, app.onError);
     },
+	sendData: function(event) {
+		var data = stringToBytes("21113\n");
+		ble.write(global_deviceid,battery.service,battery.char,data,app.showDetailPage,app.onError);
+	},
     showMainPage: function() {
         mainPage.hidden = false;
         detailPage.hidden = true;
@@ -151,7 +84,7 @@ var app = {
         mainPage.hidden = true;
         detailPage.hidden = false;
     },
-    onError: function(reason) {
-        alert("ERROR: " + reason); // real apps should use notification.alert
+	onError: function(reason) {
+       // alert("ERROR: " + reason); // real apps should use notification.alert
     }
 };
