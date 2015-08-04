5cc6f3340581f0057aa059764dbded298d531690
Ashwin Singh Tomar <makert40@gmail.com>
2015-08-04 12:50:06 +0530

    201508041250

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-var db;
+
 angular.module('starter.controllers', [])
 
 .controller('HomeTabCtrl', function($scope) {
@@ -11,17 +11,6 @@ angular.module('starter.controllers', [])
 	$scope.data.pickup3 = 2;
 	$scope.data.pickup4 = 3;
 	
-	document.addEventListener("deviceready", onDeviceReady, false);
-	function onDeviceReady() {	
-		//Please place your db file in wwww directory ,here example : projectpath/www/santosh.db
-		db = window.sqlitePlugin.openDatabase({name: "fragmaint.db", createFromLocation: 1});
-		alert("conncting"); 
-		db.transaction(function(tx) {
-			tx.executeSql('CREATE TABLE IF NOT EXISTS schedule_table (id integer primary key, schedulename text)');
-			tx.executeSql('INSERT INTO schedule_table (schedulename) VALUES ("Schedule_1")');
-			alert("inserting"); 
-		});
-	}
 })
 
 .controller('settingCTRL', function($scope) {
@@ -29,18 +18,5 @@ angular.module('starter.controllers', [])
 })
 
 .controller('scheduleCTRL', function($scope) {
-	db.transaction(function(tx) {
-		tx.executeSql('SELECT * FROM schedule_table',[],querySuccess,errorCB);
-	});
 	
-	function querySuccess(tx,result){
-		$.each(result.rows,function(index){
-			var row = result.rows.item(index);
-			alert(row['schedulename']);
-			$('#elist').append('<a class="item">'+row['schedulename']+'</a>');
-		});
-	}
-	function errorCB(err) {
-		alert("Error processing SQL: "+err.code);
-	}
 });
